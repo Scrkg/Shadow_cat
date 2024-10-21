7b0473ef2a6251e68c497cecbf0a11f419211f95
dam2 <dam2@a223e13.dc.domenicoscarlatti.es>
2024-10-21 21:01:57 +0200

    Documentación inicial

---
+++ src/shadow_cat/Shadow_cat.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package shadow_cat;
+
+/**
+ *
+ * @author dam2
+ */
+public class Shadow_cat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        // TODO code application logic here
+    }
+    
+}
