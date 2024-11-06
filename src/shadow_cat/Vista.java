33b7abd7c1dcd558bc7f1446518250f550e1ef93
dam2 <dam2@a223e13.dc.domenicoscarlatti.es>
2024-11-06 19:35:55 +0100

    06/11

--- src/shadow_cat/Vista.java
+++ src/shadow_cat/Vista.java
@@ -11,11 +11,17 @@ public class Vista extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Dam2\\Downloads\\Pantalla principal.png")); // NOI18N
+        getContentPane().add(jLabel1, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
